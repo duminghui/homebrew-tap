b0f4358b92749c8e5ed8264292bb583a730060f0
duminghui <dumh@jolgoo.com>
2016-07-24 01:44:33 +0800

    fix sha256

--- Casks/open-nvim-4-ide-app.rb
+++ Casks/open-nvim-4-ide-app.rb
@@ -1,6 +1,6 @@
 cask 'open-nvim-4-ide-app' do
   version '1.0.0'
-  sha256 'ef587d076d39fbc443895fa29c87231995c6d8c6'
+  sha256 '00de5aa9b3917c63d9185cdcbb524a02e357ab890e3f38cc7d2218a7688ffa41'
 
   url "https://github.com/duminghui/open-x-vim-4-ide/archive/v#{version}.tar.gz"
   name "Open Nvim for IDE"
