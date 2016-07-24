6392cb5cec0634b8159b9aa27ed2833d9ff60d8d
duminghui <dumh@jolgoo.com>
2016-07-24 12:52:55 +0800

    modify download url

--- Casks/open-nvim-4-ide-app.rb
+++ Casks/open-nvim-4-ide-app.rb
@@ -1,11 +1,11 @@
 cask 'open-nvim-4-ide-app' do
   version '1.0.1'
-  sha256 'd6c2953fed374687c6491b26365814ef1279912658b5616da7c4042545d3e8c7'
+  sha256 '3cf3a64270f6d038b33104e1e0ee61c53fd4c0bd60d920a177fdba98d3b3ca52'
 
-  url "https://github.com/duminghui/open-x-vim-4-ide/archive/v#{version}.tar.gz"
+  url "https://github.com/duminghui/open-x-vim-4-ide/raw/master/open-nvim-4-ide-app.tar.gz"
   name "Open Nvim for IDE"
   homepage 'https://github.com/duminghui/open-x-vim-4-ide'
   license :bsd
 
-  app "open-x-vim-4-ide-#{version}/open-nvim-4-ide.app"
+  app "open-x-vim-4-ide-app/open-nvim-4-ide.app"
 end
