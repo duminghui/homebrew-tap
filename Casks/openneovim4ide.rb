e7ee6860c434bacb03925cea299de63cad05018a
duminghui <dumh@jolgoo.com>
2016-07-30 10:09:57 +0800

    test

---
+++ Casks/openneovim4ide.rb
@@ -0,0 +1,12 @@
+cask 'openneovim4ide' do
+  version '1.1.0'
+  sha256 'c29109a20834bbf2956012531a4465467cb98dc36cbda8c40edcb1489dbf163b'
+
+  url "https://github.com/duminghui/open-x-vim-4-ide/releases/download/v#{version}/OpenNeoVim4IDE.tar.gz"
+  name "Open NeoVim for IDE"
+  homepage 'https://github.com/duminghui/open-x-vim-4-ide'
+  license :gpl
+
+  auto_updates true
+  app "OpenNeoVim4IDE.app"
+end
