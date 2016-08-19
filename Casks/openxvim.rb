6abe748e6650c569dd0b304292e127c2d91409cf
duminghui <dumh@jolgoo.com>
2016-08-19 14:08:06 +0800

    openxvim a big modify

--- Casks/openxvim.rb
+++ Casks/openxvim.rb
@@ -1,18 +1,18 @@
 cask 'openxvim' do
-  version '2.0.2'
-  sha256 'f6b6d94da6ba57b11a60f087c8e20519f0feadb690952fd952bba569af41a926'
+  version '2.1.0'
+  sha256 'e8baf897d632846e179083d1db7a362673b22cb5d26a43520d6616887cd07711'
 
   url "https://github.com/duminghui/open-x-vim/releases/download/v#{version}/OpenXVim.tgz"
-  name "Open X Vim In iTerm"
+  name 'Open X Vim In iTerm'
   homepage 'https://github.com/duminghui/open-x-vim'
   license :gpl
 
   auto_updates true
 
   depends_on cask: 'iterm2'
-  depends_on formula: 'tmux'
+  depends_on formula 'tmux' => :optional
   depends_on formula: 'neovim/neovim/neovim'
 
-  app "OpenNeoVim.app"
-  app "OpenVim.app"
+  app 'OpenNeoVim.app'
+  app 'OpenVim.app'
 end
