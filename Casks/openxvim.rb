271ec31ac22c08add29e84450f9d0177af61f9e2
duminghui <dumh@jolgoo.com>
2016-08-20 21:14:36 +0800

    modify

--- Casks/openxvim.rb
+++ Casks/openxvim.rb
@@ -1,6 +1,6 @@
 cask 'openxvim' do
-  version '2.2.1'
-  sha256 'efc465651f11d39d4ad2a45386e3805a57d22ce721fd5e7b867f97bdf5cc41d3'
+  version '2.2.2'
+  sha256 'bad38d5a2efa09aa63c2043919dd35efc259f776b00d5b82ad30611dcad41be4'
 
   url "https://github.com/duminghui/open-x-vim/releases/download/v#{version}/OpenXVim.tgz"
   name 'Open X Vim In iTerm'
