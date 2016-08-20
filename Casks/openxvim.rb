5925c27dd3498719ab57c142a46c03ce11365d65
duminghui <dumh@jolgoo.com>
2016-08-20 18:00:42 +0800

    fix openxvim when first open neovim window size bug

--- Casks/openxvim.rb
+++ Casks/openxvim.rb
@@ -1,6 +1,6 @@
 cask 'openxvim' do
-  version '2.2.0'
-  sha256 '7f2e50af8450594b80eef6c16cd17addfb2dbac59573a63cd77fcdd4e0db3d50'
+  version '2.2.1'
+  sha256 'efc465651f11d39d4ad2a45386e3805a57d22ce721fd5e7b867f97bdf5cc41d3'
 
   url "https://github.com/duminghui/open-x-vim/releases/download/v#{version}/OpenXVim.tgz"
   name 'Open X Vim In iTerm'
