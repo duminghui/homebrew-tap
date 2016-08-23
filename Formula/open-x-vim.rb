a99b28f29d5a8aa474e3e4a491f5e364b2ac47c9
duminghui <dumh@jolgoo.com>
2016-08-23 22:30:54 +0800

    v3.1.2

--- Formula/open-x-vim.rb
+++ Formula/open-x-vim.rb
@@ -1,9 +1,9 @@
 class OpenXVim < Formula
   desc "Open files with Vim/NeoVim"
   homepage 'https://github.com/duminghui/open-x-vim'
-  version '3.1.1'
+  version '3.1.2'
   url "https://github.com/duminghui/open-x-vim.git", :tag => "v#{version}",
-    :revision => "81dab81983cba118e06cd7da4fb7476db238bcf8"
+    :revision => "82769230582491a3340d4da0b38d1473af1f609b"
   # url "https://github.com/duminghui/open-x-vim/archive/v#{version}.tar.gz"
   # sha256 "c594d6dbeb5276eeb1e2382e5d789976f9d5015264b0f415b7bee0fb3b2902df"
   head "https://github.com/duminghui/open-x-vim.git"
