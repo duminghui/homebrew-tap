3497d89db6e7a6e15de8a864a27af8773d6cd378
duminghui <dumh@jolgoo.com>
2016-08-23 15:44:27 +0800

    add formula

---
+++ Formula/open-x-vim.rb
@@ -0,0 +1,19 @@
+class OpenXVim < Formula
+  desc "Open files with Vim/NeoVim"
+  homepage 'https://github.com/duminghui/open-x-vim'
+  version '3.1.0'
+  url "https://github.com/duminghui/open-x-vim/archive/v#{version}.tar.gz"
+  sha256 "c594d6dbeb5276eeb1e2382e5d789976f9d5015264b0f415b7bee0fb3b2902df"
+  head "https://github.com/duminghui/open-x-vim.git"
+
+  depends_on :xcode => :build
+  depends_on 'tmux' => :optional
+  depends_on 'neovim/neovim/neovim' => :optional
+
+  def install
+    xcodebuild "-configuration","Release","-project","OpenXVim/OpenXVim.xcodeproj","clean"
+    xcodebuild "-configuration","Release","-project","OpenXVim/OpenXVim.xcodeproj"
+
+    prefix.install "OpenXVim/build/Release/OpenXVim.app"
+  end
+end
